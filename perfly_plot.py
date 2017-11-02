142929c241efaa7b4564385c4d6e27250537aae5
Klubix <klubix36@gmail.com>
2017-11-02 16:02:08 +0100

    Update of plot diagram and object naming.

--- perfly_plot.py
+++ perfly_plot.py
@@ -3,39 +3,51 @@ import os
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+
 class PerflyPlot(object):
 
     def __init__(self):
         self._dates_list = list()
         self._results_list = list()
-        self._perlfy_plot_data = dict()
+        self._perfly_plot_data = dict()
+        self._perfly_data_file = "perfly_data.yaml"
+        self._perfly_plot_png_file = "perfly_plot.png"
+        self._perfly_results_key = "perfly_results"
+        self._card_processing_avg_key = "card_processing_avg"
+        self._card_processing_avg_limits_key = "card_processing_avg_limit"
+        self._static_files_folder = "static"
+        self._perfly_plot_date_time_format = "%Y%m%d%H%M"
+        self._perfly_plot_legend_data_label = "Card processing avg"
+        self._perfly_plot_legend_limit_label = "AVG limit"
+        self._perfly_plot_title = "PERFLY CARD PROCESSING AVG OVER TIME"
 
     def load_perfly_data(self):
-        with open("perfly_data.yaml", 'r') as perfly_data_file:
+        with open(self._perfly_data_file, 'r') as perfly_data_file:
             self._perfly_plot_data = yaml.load(perfly_data_file)
 
     def save_new_perfly_data(self, date_string, card_processing_avg):
-        self._perfly_plot_data['perfly_results'][str(date_string)] = dict()
-        self._perfly_plot_data['perfly_results'][str(date_string)]['card_processing_avg'] = \
+        self._perfly_plot_data[self._perfly_results_key][str(date_string)] = dict()
+        self._perfly_plot_data[self._perfly_results_key][str(date_string)][self._card_processing_avg_key] = \
             float(card_processing_avg)
-        with open("perfly_data.yaml", 'w') as perfly_data_file:
+        with open(self._perfly_data_file, 'w') as perfly_data_file:
             yaml.dump(self._perfly_plot_data, perfly_data_file)
 
     def prepare_axes_data(self):
-        for perfly_result in self._perfly_plot_data['perfly_results']:
-            perfly_date_time = datetime.strptime(str(perfly_result), "%Y%m%d%H%M")
-            card_processing_avg_result = self._perfly_plot_data['perfly_results'][perfly_result]['card_processing_avg']
+        for perfly_result in self._perfly_plot_data[self._perfly_results_key]:
+            perfly_date_time = datetime.strptime(str(perfly_result), self._perfly_plot_date_time_format)
+            card_processing_avg_result = \
+                self._perfly_plot_data[self._perfly_results_key][perfly_result][self._card_processing_avg_key]
             self._dates_list.append(perfly_date_time)
             self._results_list.append(card_processing_avg_result)
 
     def create_plot(self):
         plt.clf()
         plt.figure(figsize=(20, 6))
-        plt.plot(self._dates_list, self._results_list, 'ro', label="Card processing avg")
-        plt.axhline(y=self._perfly_plot_data['card_processing_avg_limit'], label="AVG limit")
-        # box = plt.get_position()
-        # plt.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=2, fancybox=True)
-        plt.title("PERFLY CARD PROCESSING AVG OVER TIME")
+        plt.plot(self._dates_list, self._results_list, 'ro', label=self._perfly_plot_legend_data_label)
+        plt.axhline(y=self._perfly_plot_data[self._card_processing_avg_limits_key],
+                    label=self._perfly_plot_legend_limit_label)
+        plt.legend(loc='best', bbox_to_anchor=(0.2, -0.3), ncol=2, fancybox=True)
+        plt.title(self._perfly_plot_title)
         plt.gcf().autofmt_xdate()
-        plt.savefig(os.path.join('static', 'perfly_plot.png'), bbox_inches='tight')
+        plt.xticks(self._dates_list, rotation='vertical')
+        plt.savefig(os.path.join(self._static_files_folder, self._perfly_plot_png_file), bbox_inches='tight')
--- perfly_plot_app.py
+++ perfly_plot_app.py
@@ -11,11 +11,11 @@ def perfly_plot_rest():
         date_string = request.args.get("date")
         card_processing_avg = request.args.get("cardProcessingAVG")
 
-        perfly_plot = PerflyPlot()
-        perfly_plot.load_perfly_data()
-        perfly_plot.save_new_perfly_data(date_string, card_processing_avg)
-        perfly_plot.prepare_axes_data()
-        perfly_plot.create_plot()
+        perfly_plot_instance = PerflyPlot()
+        perfly_plot_instance.load_perfly_data()
+        perfly_plot_instance.save_new_perfly_data(date_string, card_processing_avg)
+        perfly_plot_instance.prepare_axes_data()
+        perfly_plot_instance.create_plot()
 
         return "Data added to the perfly plot."
     else:
@@ -23,8 +23,8 @@ def perfly_plot_rest():
 
 
 if __name__ == "__main__":
-    perfly_plot = PerflyPlot()
-    perfly_plot.load_perfly_data()
-    perfly_plot.prepare_axes_data()
-    perfly_plot.create_plot()
+    perfly_plot_instance = PerflyPlot()
+    perfly_plot_instance.load_perfly_data()
+    perfly_plot_instance.prepare_axes_data()
+    perfly_plot_instance.create_plot()
     perfly_plot_app.run(threaded=True)
